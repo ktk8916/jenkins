8756f43cbd5f774aed86768f4f3c016196e94afc
ktk8916 <ktk8916@naver.com>
2023-03-24 20:11:04 +0900

    init

--- src/test/java/study/jenkins/JenkinsApplicationTests.java
+++ src/test/java/study/jenkins/JenkinsApplicationTests.java
@@ -3,7 +3,7 @@ package study.jenkins;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class JenkinsApplicationTests {
 
 	@Test
